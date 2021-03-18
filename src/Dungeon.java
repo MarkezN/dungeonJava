aea76efa151f89bd5aa90bc77aa4eb0ff4952dfb
marquez <marknovak85@gmail.com>
2021-03-18 22:16:14 +0100

    made v object with random moves

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,5 +1,6 @@
 /* import java.util.Scanner; */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dungeon {
@@ -7,10 +8,12 @@ public class Dungeon {
     private int length;
     private int width;
     private Player p;
+    //private Vampire v;
     private Scanner scan;
     private int vampires;
     private int moves;
     private boolean vampiresMove;
+    private ArrayList<Vampire> vampirus;
 
     public Dungeon(int length, int width, int vampires, int moves, boolean vampiresMove) {
         this.length = length;
@@ -20,7 +23,20 @@ public class Dungeon {
         this.vampiresMove = vampiresMove;
         this.dungeon = new char[length][width];
         this.p = new Player('@');
+        this.vampirus = new ArrayList(this.vampires);
         this.scan = new Scanner(System.in);
+    }
+
+    public boolean vampMove(boolean vampiresMove) {
+
+        for (Vampire v : this.vampirus) {
+            if (!vampiresMove) {
+                v.setX(0);
+                v.setY(0);
+                return true;
+            }
+        }
+        return false;
     }
 
     public void drawDungeon(char[][] dungeon) {
@@ -46,20 +62,49 @@ public class Dungeon {
         }
     }
 
-    public int getLength() {
-        return length;
+    public void makeVamp(ArrayList<Vampire> vampirus) {
+        for (int i = 0; i < this.vampires; i++) {
+            Vampire v = new Vampire('v');
+            vampirus.add(v);
+        }
+    }
+
+    public void vampList(ArrayList<Vampire> vampirus) {
+        for (Vampire v : vampirus) {
+            System.out.println(v);
+        }
+    }
+
+    public void vampMove(char[][] dungeon) {
+        for (Vampire v : this.vampirus) {
+            v.move(dungeon, scan);
+        }
     }
 
     public void run() {
+
+        //this method can be way refactored :)
+        makeVamp(vampirus);
+
+        vampMove(dungeon);
+
         System.out.println(this.moves);
+        System.out.println();
         System.out.println(p.toString());
+        vampList(vampirus);
         p.moveStart(dungeon);
+        System.out.println();
         this.moves--;
         while (this.moves >= 0) {
             drawDungeon(dungeon);
             clearDungeon(dungeon);
             p.move(dungeon, scan);
+            vampMove(dungeon);
             System.out.println(this.moves);
+            System.out.println();
+            System.out.println(p.toString());
+            vampList(vampirus);
+            System.out.println();
             this.moves--;
         }
         System.out.println("You LOOOOOSE :)");
--- src/Vampire.java
+++ src/Vampire.java
@@ -1,15 +1,66 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Vampire extends Charact implements Movable {
-
+    private int x = 0;
+    private int y = 0;
+    private char name;
+    private Random r;
+    
     public Vampire(char name) {
-        super(name);
-
+        super(name);                              
+        this.name = name;
+        this.y = y;
+        this.x = x;
+        this.r = new Random();
     }
-
 
     @Override
     public void move(char[][] ch, Scanner scan) {
+                 //randomizirati koordinate i mozda posle stvoriti novi objekat sa novim koordinatama
+      //  this.x =
+           int start = 0;
+           int end = ch.length;
 
+           this.x = r.nextInt(end - start) + start;
+           this.y = r.nextInt(end - start) + start;
+           ch[getX()][getY()] = getName();
+        
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public char getName() {
+        return name;
+    }
+
+    public void setName(char name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " " +  getY() + " " + getX();
     }
 }
+
+
+
+
+
+
+
